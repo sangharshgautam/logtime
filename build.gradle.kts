b735ca8d2d66b4fa49cd76fd3e017b67dffde10a
sangharsh <sangharsh_iitd@yahoo.com>
2026-06-06 17:14:53 +0100

    deprecations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
@@ -5,13 +6,29 @@ plugins {
     id("org.jetbrains.intellij.platform")
     id("org.jetbrains.changelog")
 }
-
+// --- ADDED THIS BLOCK TO TARGET A SINGLE VERSION ---
+intellijPlatform {
+    pluginVerification {
+        ides {
+            // This forces the verifier to ONLY test against 2025.2.6.2
+            // Targets only your exact version explicitly
+            select {
+                // Pin it to IntelliJ IDEA Ultimate or Community matching your exact build
+                types.set(listOf(IntelliJPlatformType.IntellijIdea))
+                version = "2026.1.3"
+//                sinceBuild.set("2025.2.6.2")
+//                untilBuild.set("2025.2.6.2")
+            }
+        }
+    }
+}
+// --------------------------------------------------
 dependencies {
     testImplementation("junit:junit:4.13.2")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2025.2.6.2")
+        intellijIdea("2026.1.3")
         testFramework(TestFrameworkType.Platform)
     }
 }
